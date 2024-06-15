33cbb391d7d15d8c4a15caee7dd8da2a274c493d
ChubYury <yurichubuser@gmail.com>
2024-06-15 14:59:06 +0300

    start server

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+app.get('/', (_req, res) => res.send('Hello server'))
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then((_res) => {
+    console.log('-------------------');
+    console.log('Connect mongoDB');
+    app.listen(process.env.PORT, err => {
+      err ? console.log(err) : console.log('Express listeningpot: ' + process.env.PORT);
+      console.log('-------------------');
+    })
+  }).catch((err => console.log(err)))
