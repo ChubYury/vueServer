c6a55dfb450c022770a7ec67e4d7849cccbdd2f3
ChubYury <yurichubuser@gmail.com>
2024-07-29 20:53:44 +0300

    added setCoache route

--- routes/api-coaches-routes.js
+++ routes/api-coaches-routes.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { getCoaches } = require('../controllers/api-coaches-controller.js');
-const mainHeader = require('../helpers/headerHandler.js')
+const { getCoaches,
+        setCoache } = require('../controllers/api-coaches-controller.js');
+const {
+        mainHeader,
+        postHeader,
+      } = require('../helpers/headerHandler.js')
 
-router.get('/api/coaches', mainHeader, getCoaches);
+router.get('/api/getCoaches', mainHeader, getCoaches);
+
+router.use('/api/setCoache', postHeader);
+router.post('/api/setCoache', setCoache);
 
 module.exports = router
