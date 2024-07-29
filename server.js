40b47e09d856c6a09a2357adfd3ac8b9d0fd01fe
ChubYury <yurichubuser@gmail.com>
2024-07-29 20:55:12 +0300

    connect middlewares

--- server.js
+++ server.js
@@ -8,16 +8,19 @@ const routes = require('./routes')
 const morgan = require ( 'morgan' )
 app.use(morgan(':remote-addr[:date[clf]]" :method :url HTTP/:http-version" :status :req[content-length] :res[content-length]'));
 
+app.use(express.urlencoded({ extended: false })) 
+app.use(express.json())
+
 app.use(routes);
 app.get('/', (_req, res) => res.send('Hello server'));
 
 mongoose
-  .connect(process.env.MONGO_URL)
-  .then((_res) => {
+.connect(process.env.MONGO_URL)
+.then((_res) => {
+  console.log('-------------------');
+  console.log('Connect mongoDB');
+  app.listen(process.env.PORT, err => {
+    err ? console.log(err) : console.log('Express listeningpot: ' + process.env.PORT);
     console.log('-------------------');
-    console.log('Connect mongoDB');
-    app.listen(process.env.PORT, err => {
-      err ? console.log(err) : console.log('Express listeningpot: ' + process.env.PORT);
-      console.log('-------------------');
-    });
-  }).catch((err => console.log(err)));
+  });
+}).catch((err => console.log(err)));
