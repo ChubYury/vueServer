cc2c9c9d0e4f39b0b65006ce200626bb8d42c404
ChubYury <yurichubuser@gmail.com>
2024-09-04 14:43:31 +0300

    added signup and login routes

--- routes/api-user-routes.js
+++ routes/api-user-routes.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const { getUsers } = require('../controllers/user-controller.js');
+const {
+  getUsers,
+  setUser,
+  userLogin
+ } = require('../controllers/user-controller.js');
+const { postHeader } = require('../helpers/headerHandler.js');
 
 router.get('/api/users', getUsers);
+
+router.use('/api/signup', postHeader);
+router.post('/api/signup', setUser);
+
+router.use('/api/login', postHeader);
+router.post('/api/login', userLogin)
 
 module.exports = router
