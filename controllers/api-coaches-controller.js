db1495ac6e828f521d137b252c63bc74af71eabb
ChubYury <yurichubuser@gmail.com>
2024-09-07 18:23:10 +0300

    added checking for existing coach

--- controllers/api-coaches-controller.js
+++ controllers/api-coaches-controller.js
@@ -1,23 +1,26 @@
 const mongoose = require('mongoose');
 const Coache = mongoose.model('Coache');
 
-const getCoaches = (_req, res) => {
+const getCoaches = async (_req, res) => {
   
-  Coache
+  await Coache
     .find()
     .exec()
     .then(coaches => res.status(200).json(coaches))
     .catch(err => console.log(err));
 };
 
-const setCoache = (req, res) => {
-  const {firstName, lastName, areas, description, hourlyRate} = req.body;
-  const coache = new Coache({firstName, lastName, areas, description, hourlyRate});
+const setCoache = async (req, res) => {
+  const {firstName, lastName, areas, description, hourlyRate, userId} = req.body;
+  const coache = new Coache({firstName, lastName, areas, description, hourlyRate, userId});
+  const findCoach = await Coache.findOne({userId});
+
+  if (findCoach) return res.status(400).json({ mess: 'Coach exist'});
   
-  coache
-    .save()
-    .then((newCoache) => res.status(200).json(newCoache))
-    .catch((err) => console.log(err));
+  await coache
+        .save()
+        .then((newCoache) => res.status(200).json(newCoache))
+        .catch((err) => console.log(err));
 }
 
 module.exports = {
