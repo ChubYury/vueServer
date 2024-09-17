4d7b82d70b5ff01a8fa99fc3e4fccdc7cb8a985b
ChubYury <yurichubuser@gmail.com>
2024-09-17 10:30:01 +0300

    added use middleware

--- server.js
+++ server.js
@@ -3,14 +3,10 @@ const app = express();
 const mongoose = require('mongoose');
 require('dotenv').config();
 require('./models');
-const routes = require('./routes')
+const middlewares = require('./middlewares');
+const routes = require('./routes');
 
-const morgan = require ( 'morgan' )
-app.use(morgan(':remote-addr[:date[clf]]" :method :url HTTP/:http-version" :status :req[content-length] :res[content-length]'));
-
-app.use(express.urlencoded({ extended: false })) 
-app.use(express.json())
-
+app.use(middlewares);
 app.use(routes);
 app.get('/', (_req, res) => res.send('Hello server'));
 
